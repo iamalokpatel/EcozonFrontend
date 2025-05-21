8414a633230908033a7b73b6caf3bf1940827da6
Alok Patel <alokpatel23273@gmail.com>
2025-05-21 13:53:30 +0530

    commited

--- app/products/page.js
+++ app/products/page.js
@@ -81,7 +81,7 @@ export default function CategoryPage() {
           No products found in this category.
         </p>
       ) : (
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {products
             .filter(
               (product) =>
