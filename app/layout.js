bfeb0d53f1cf42cd469ea72dbd39db7b67c48850
Alok Patel <alokpatel23273@gmail.com>
2025-05-23 20:37:02 +0530

    commited

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,6 @@ import "./globals.css";
 import React from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
